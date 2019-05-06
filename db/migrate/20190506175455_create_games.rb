fe48092df3df39f1a87c8492f8481b123624870d
KingDad <pgrcarlson@gmail.com>
2019-05-06 15:01:59 -0400

    Added Games table

--- db/migrate/20190506175455_create_games.rb
+++ db/migrate/20190506175455_create_games.rb
@@ -1,6 +1,9 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
+      t.string :title, null: false
+      t.text :description
+      t.integer :max_players
     end
   end
 end
