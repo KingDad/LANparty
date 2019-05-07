691a9700627abbe48aa270422907f6100e702cf0
KingDad <pgrcarlson@gmail.com>
2019-05-07 15:47:10 -0400

    Got basic API call to return properly

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
 
+const axios = require('axios');
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
+
 class App extends Component {
   constructor(props){
     super(props);
   }
 
   componentDidMount(){
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    fetch("https://api-v3.igdb.com/keywords",{
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'user-key': '26085dfadb969be0919afbc361218dc2',
-      },
-      data: "fields created_at,name,slug,updated_at,url;"
-    })
-      .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          let errorMessage = `${response.status}(${response.statusText})` ,
-          error = new Error(errorMessage);
-          throw(error);
-        }
-      })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
-      })
-      .catch(error => console.error(`Error in fetch: ${error.message}`));
+
+
+    axios({
+  url: corsProxy + "https://api-v3.igdb.com/games/?search=street%20fighter&fields=name",
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'user-key': '26085dfadb969be0919afbc361218dc2'
+  },
+  data: "fields name;"
+})
+  .then(response => {
+      console.log(response.data);
+  })
+  .catch(err => {
+      console.error(err);
+  });
   }
 
   render(){
     return(
-      <p>We did it</p>
+      <p>We did it, bois!</p>
     )
   }
 }
