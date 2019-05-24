0b7567d81c2acf1e1319153a6a77942bb08227f1
KingDad <pgrcarlson@gmail.com>
2019-05-23 20:41:35 -0400

    Added form validations for the required fields in the Event create and edit forms

--- app/javascript/react/containers/EventEdit.js
+++ app/javascript/react/containers/EventEdit.js
@@ -7,6 +7,7 @@ import DateTimeField from '../components/DateTimeField'
 class EventEdit extends Component {
   constructor(props){
     super(props)
+    this.validateSubmission = this.validateSubmission.bind(this)
     this.takeChange = this.takeChange.bind(this)
     this.sendUpdates = this.sendUpdates.bind(this)
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
@@ -18,7 +19,47 @@ class EventEdit extends Component {
       description: "",
       eventDateTime: "",
       twitchStream: "",
-      gameIDs: []
+      gameIDs: [],
+      errors: {}
+    }
+  }
+
+  validateSubmission(submission){
+    let titlePresent;
+    let dateTimePresent;
+    if(submission.title.trim() === "" && submission.event_datetime.trim() === ""){
+      let newError = {emptyTitle: "Please provide an event title before submitting", emptyDateTime: "Please provide a date and time before submitting"};
+      this.setState({errors: newError});
+      titlePresent = false;
+      dateTimePresent = false;
+    }
+    else if (submission.title.trim() === ""){
+      let newError = {emptyTitle: "Please provide an event title before submitting"};
+      this.setState({errors: newError});
+      titlePresent = false;
+      let errorState = this.state.errors;
+      delete errorState.emptyDateTime;
+    }
+    else if(submission.event_datetime.trim() === ""){
+      let newError = {emptyDateTime: "Please provide a date and time before submitting"};
+      this.setState({errors: newError});
+      dateTimePresent = false;
+    }
+    else{
+      let errorState = this.state.errors;
+      delete errorState.emptyTitle;
+      delete errorState.emptyDateTime;
+      this.setState({errors: errorState});
+      titlePresent = true;
+      dateTimePresent = true;
+    }
+
+    if (titlePresent === true && dateTimePresent === true){
+      return true
+    }
+    else{
+      console.log(this.state.errors);
+      return false
     }
   }
 
@@ -64,7 +105,9 @@ class EventEdit extends Component {
       twitch_stream: this.state.twitchStream,
       playables: this.state.gameIDs
     }
-    this.sendUpdates(formPayload);
+    if(this.validateSubmission(formPayload)){
+      this.sendUpdates(formPayload);
+    }
   }
 
   addGame(gameID){
@@ -119,6 +162,17 @@ class EventEdit extends Component {
   render(){
     let gameTiles
     let title
+    let errorDiv
+    let errorItems
+
+    if(Object.keys(this.state.errors).length > 0){
+      errorItems = Object.values(this.state.errors).map(error =>{
+        return(
+          <li key={error}>{error}</li>
+        );
+      })
+      errorDiv = <div>{errorItems}</div>;
+    }
 
     if (this.state.gameIDs.length > 0){
       gameTiles = this.state.gameIDs.map((ID) =>{
@@ -135,6 +189,7 @@ class EventEdit extends Component {
       <div className="container">
         {title}
         <form id="event-form" onSubmit={this.handleFormSubmit} action="localhost:3000/events">
+          {errorDiv}
           <TextField name="title" id="title" label="Title:" content={this.state.title} handleChange={this.takeChange} />
           <TextField name="description" id="description" label="Description:" content={this.state.description} handleChange={this.takeChange} />
           <TextField name="twitchStream" id="twitchStream" label="Name of Twitch Stream:" content={this.state.twitchStream} handleChange={this.takeChange} />
